c75bc239a39017905da50adb95bd71863529f9fb
Prashant Kolhar <60572402+Kolhar-Prashant@users.noreply.github.com>
2020-05-07 11:18:42 +0530

    Update Sieve_of_Eratosthenes.py

--- Sieve_of_Eratosthenes.py
+++ Sieve_of_Eratosthenes.py
@@ -1,7 +1,6 @@
 import math  #to calcuate square root the limit
 
 def prime_sieve(n,sqrt):
-
     for e in range(2,sqrt+1):   #looping from 2 till square root of limit
         for i in range(e,n+1):   #looping from current iteration to limit
             c=i*e                 # Calculating the product of current element till limit
@@ -9,7 +8,6 @@ def prime_sieve(n,sqrt):
                 break                #breaking the current element iteration
             else:
                 L[c]=False   # Else setting the True Flag to False of the product elements
-
     for num in range(2, n+1):  # Looping from 2 till limit
         if L[num] == True:     # If the Flag is True
             prime_List.append(num)
