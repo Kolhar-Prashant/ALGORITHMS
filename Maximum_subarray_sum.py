c3e1d9bbf94a414007d07608c14f47b239d7bd04
Prashant Kolhar <60572402+Kolhar-Prashant@users.noreply.github.com>
2020-05-24 22:04:32 +0530

    Update Maximum_subarray_sum.py

--- Maximum_subarray_sum.py
+++ Maximum_subarray_sum.py
@@ -1,21 +1,15 @@
 # Calculating Maximum subarray sum using Kadane's Algorithm.
-def Kadane_Max_subarray_sum():
-    curr_sum=0
-    max_sum=0
-    temp=0
-    for e in L:
-        temp=curr_sum
-        curr_sum+=e
-        if curr_sum < 0:
-            curr_sum=0
-            continue
-        if temp > curr_sum:
-            if temp > max_sum:
-                max_sum=temp
-        else:
-            if curr_sum > max_sum:
-                max_sum=curr_sum
-    print(max_sum)
-n = int(input("Enter array size"))
-L = list(map(int, input().split()))
-Kadane_Max_subarray_sum()
+def Kadane(L):
+   ms=0
+   cs=0
+   for e in L:              
+       sum = cs + e       
+       if (cs + e) < 0:
+           cs=0
+       else:
+           cs=sum
+       if cs > ms:
+           ms=cs
+   print("Max sum :",ms)     
+L=[-4,1,3,-2,6,2,-1,-4,-7] #provide array values here.
+Kadane(L)
